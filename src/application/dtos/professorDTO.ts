365294733ee3ba14889ec5d8f1dc5488f382c8af
LFerriVM <lucasfvm60@gmail.com>
2026-02-11 15:35:27 -0300

    add: adding dto on application layer

---
+++ src/application/dtos/professorDTO.ts
@@ -0,0 +1,11 @@
+class ProfessorDTO {
+    id: number
+    name: string
+    email: string
+    registration: string
+    createdAt: Date
+    updatedAt: Date
+    status: boolean
+    constructor() { }
+}
+export default ProfessorDTO
