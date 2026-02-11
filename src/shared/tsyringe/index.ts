b9baca48300da5bf027b8a90394b3a584590aa61
LFerriVM <lucasfvm60@gmail.com>
2026-02-11 15:32:23 -0300

    add: adding more singletons on DI container

--- src/shared/tsyringe/index.ts
+++ src/shared/tsyringe/index.ts
@@ -1,4 +1,20 @@
-import CoordinatorController from "@/api/controllers/coordinatorControlller";
+import BaseController from "api/controllers/baseController";
+import CreateProfessorController from "api/controllers/professor/createProfessorController";
+import DeleteProfessorController from "api/controllers/professor/deleteProfessorController";
+import GetAllProfessorsController from "api/controllers/professor/getAllProfessorsController";
+import GetProfessorByIdController from "api/controllers/professor/getProfessorByIdController";
+import UpdateProfessorController from "api/controllers/professor/updateProfessorController";
+import IProfessorServices from "application/interfaces/iProfessorServices";
+import ProfessorServices from "application/services/professorServices";
+import IProfessorRepository from "domain/repositories/iProfessorRepository";
+import ProfessorRepository from "infrastructure/repositories/professorRepository";
 import { container } from "tsyringe";
 
-container.registerSingleton<BaseController>("CoordinatorController", CoordinatorController)
+
+container.registerSingleton<IProfessorRepository>("ProfessorRepository", ProfessorRepository)
+container.registerSingleton<IProfessorServices>("ProfessorServices", ProfessorServices)
+container.registerSingleton<BaseController>("GetAllProfessorsController", GetAllProfessorsController)
+container.registerSingleton<BaseController>("GetProfessorByIdController", GetProfessorByIdController)
+container.registerSingleton<BaseController>("CreateProfessorController", CreateProfessorController)
+container.registerSingleton<BaseController>("UpdateProfessorController", UpdateProfessorController)
+container.registerSingleton<BaseController>("DeleteProfessorController", DeleteProfessorController)
