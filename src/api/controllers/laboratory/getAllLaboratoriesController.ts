6f2950538f73451cd2e74fd352b9e973b1a0683f
izasd <izas212@gmail.com>
2026-04-02 10:45:44 -0300

    feat: add Laboratory entity controllers and routes

---
+++ src/api/controllers/laboratory/getAllLaboratoriesController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import BaseController from "../baseController";
+import ControllerExceptionThrowHelper from "api/helpers/controllerExceptionThrowHelper";
+import { inject, injectable } from "tsyringe";
+import ILaboratoryServices from "application/interfaces/iLaboratoryServices";
+
+@injectable()
+class GetAllLaboratoriesController implements BaseController {
+    constructor(
+        @inject("LaboratoryServices")
+        private readonly laboratoryServices: ILaboratoryServices
+    ) { }
+    async Handle(req: Request, res: Response) {
+        try {
+            const user = req.user
+            // ApiException.When(!user, ApiExceptionNameEnum.UNAUTHENTICATED_USER, "You are not authenticated to the API. Authenticate yourself", 401)
+            const result = await this.laboratoryServices.GetAllAsync()
+            return res.status(200).json(result)
+        } catch (ex) {
+            return ControllerExceptionThrowHelper.Throw(res, ex)
+        }
+    }
+
+}
+export default GetAllLaboratoriesController
---
+++ src/api/controllers/laboratory/updateLaboratoryController.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from "express";
+import BaseController from "../baseController";
+import ControllerExceptionThrowHelper from "api/helpers/controllerExceptionThrowHelper";
+import { inject, injectable } from "tsyringe";
+import ILaboratoryServices from "application/interfaces/iLaboratoryServices";
+import { LaboratoryDTO } from "application/dtos/laboratoryDTO";
+import CheckRequestPropertiesHelper from "api/helpers/checkRequestPropertiesHelper";
+import { ValidationError } from "sequelize";
+
+@injectable()
+class UpdateLaboratoryController implements BaseController {
+    constructor(
+        @inject("LaboratoryServices")
+        private readonly laboratoryServices: ILaboratoryServices
+    ) { }
+    async Handle(req: Request, res: Response) {
+        try {
+            const { name, storageSpace, maxOccupants, description } = req.body
+            const { id } = req.params as unknown as { id: number }
+            CheckRequestPropertiesHelper.CheckRequired({ id, name, storageSpace, maxOccupants, description })
+            const user = req.user
+            // ApiException.When(!user, ApiExceptionNameEnum.UNAUTHENTICATED_USER, "You are not authenticated to the API. Authenticate yourself", 401)
+            const laboratoryDTO: LaboratoryDTO = { name, storageSpace, maxOccupants, description } as LaboratoryDTO
+            const result = await this.laboratoryServices.UpdateAsync(id, laboratoryDTO)
+            return res.status(200).json(result)
+        } catch (ex) {
+            if (ex instanceof ValidationError)
+                console.log(ex.errors)
+            return ControllerExceptionThrowHelper.Throw(res, ex)
+        }
+    }
+
+}
+export default UpdateLaboratoryController
