90c88278e453e98d72633ecff618f5859ae8e361
izasd <izas212@gmail.com>
2026-04-02 10:45:33 -0300

    feat: add complete CRUD services for Laboratory, ProjectCategory, and EquipmentCategory

--- src/application/services/LaboratoryServices.ts
+++ src/application/services/LaboratoryServices.ts
@@ -2,6 +2,9 @@ import { inject, injectable } from "tsyringe";
 import ILaboratoryServices from "application/interfaces/iLaboratoryServices";
 import { LaboratoryDTO } from "application/dtos/laboratoryDTO";
 import ILaboratoryRepository from "domain/repositories/iLaboratoryRepository";
+import { ApplicationExceptionName } from "application/constants/applicationExceptionName";
+import ApplicationException from "application/exceptions/applicationException";
+import Laboratory from "domain/models/laboratory";
 
 @injectable()
 class LaboratoryServices implements ILaboratoryServices {
@@ -10,9 +13,22 @@ class LaboratoryServices implements ILaboratoryServices {
         private readonly laboratoryRepository: ILaboratoryRepository
     ) { }
     async GetAllAsync() {
-        const result = await this.laboratoryRepository.Find() as LaboratoryDTO[]
-        return result
+        return (await this.laboratoryRepository.Find()) as LaboratoryDTO[]
     }
+    async GetByIdAsync(id: number) {
+        return (await this.laboratoryRepository.FindById(id)) as LaboratoryDTO
+    }
+    async CreateAsync(data: LaboratoryDTO) {
+        return (await this.laboratoryRepository.Create(data)) as LaboratoryDTO
+    }
+    async UpdateAsync(id: number, data: LaboratoryDTO) {
+        if (!(await this.GetByIdAsync(id) as Laboratory))
+            throw new ApplicationException(ApplicationExceptionName.NOT_FOUND, "No laboratory was found with the provided id", 404)
+        return (await this.laboratoryRepository.Update(id, data)) as LaboratoryDTO
 
+    }
+    async DeleteAsync(id: number) {
+        return (await this.laboratoryRepository.Delete(id))
+    }
 }
 export default LaboratoryServices
--- src/application/services/ProjectCategoryServices.ts
+++ src/application/services/ProjectCategoryServices.ts
@@ -2,6 +2,9 @@ import { inject, injectable } from "tsyringe";
 import IProjectCategoryServices from "application/interfaces/iProjectCategoryServices";
 import { ProjectCategoryDTO } from "application/dtos/projectCategoryDTO";
 import IProjectCategoryRepository from "domain/repositories/iProjectCategoryRepository";
+import { ApplicationExceptionName } from "application/constants/applicationExceptionName";
+import ApplicationException from "application/exceptions/applicationException";
+import ProjectCategory from "domain/models/projectCategory";
 
 @injectable()
 class ProjectCategoryServices implements IProjectCategoryServices {
@@ -10,9 +13,22 @@ class ProjectCategoryServices implements IProjectCategoryServices {
         private readonly projectCategoryRepository: IProjectCategoryRepository
     ) { }
     async GetAllAsync() {
-        const result = await this.projectCategoryRepository.Find() as ProjectCategoryDTO[]
-        return result
+        return (await this.projectCategoryRepository.Find()) as ProjectCategoryDTO[]
     }
+    async GetByIdAsync(id: number) {
+        return (await this.projectCategoryRepository.FindById(id)) as ProjectCategoryDTO
+    }
+    async CreateAsync(data: ProjectCategoryDTO) {
+        return (await this.projectCategoryRepository.Create(data)) as ProjectCategoryDTO
+    }
+    async UpdateAsync(id: number, data: ProjectCategoryDTO) {
+        if (!(await this.GetByIdAsync(id) as ProjectCategory))
+            throw new ApplicationException(ApplicationExceptionName.NOT_FOUND, "No project category was found with the provided id", 404)
+        return (await this.projectCategoryRepository.Update(id, data)) as ProjectCategoryDTO
 
+    }
+    async DeleteAsync(id: number) {
+        return (await this.projectCategoryRepository.Delete(id))
+    }
 }
 export default ProjectCategoryServices
