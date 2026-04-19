dfb8fac440b82d685c424b261ca467f5cdc88f71
LFerriVM <lucasfvm60@gmail.com>
2026-04-19 11:48:40 -0300

    feat(tests): add initial implementation of Vitest for Borrow and Professor domains tests

---
+++ tests/domain/borrow.spec.ts
@@ -0,0 +1,31 @@
+import Borrow, { IBorrow } from "@/domain/models/borrow"
+import { describe, expect, it } from "vitest"
+
+describe("Borrow Domain", () => {
+    const createData = {
+        borrowDate: new Date(),
+        equipmentId: 1,
+        isStillBorrowed: true,
+        studentId: 1,
+
+    }
+
+    const hydratedData: IBorrow = {
+        id: 1,
+        borrowDate: new Date(),
+        equipmentId: 1,
+        isStillBorrowed: true,
+        studentId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        isVisible: true
+    }
+
+    it("should create entity with correct borrow date", () => {
+        const entity = Borrow.create(createData.equipmentId, createData.borrowDate, createData.studentId, undefined, createData.isStillBorrowed)
+        expect(entity.borrowDate).toEqual(createData.borrowDate)
+        expect(entity.equipmentId).toEqual(createData.equipmentId)
+        expect(entity.isStillBorrowed).toEqual(createData.isStillBorrowed)
+        expect(entity.studentId).toEqual(createData.studentId)
+    })
+})
