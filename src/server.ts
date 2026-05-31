a15009898cfff47fb937075eab33d36a99b1907e
Nicolas <nicolasbassini@hotmail.com>
2026-05-31 19:49:12 -0300

    feature: fix proxy on production

--- src/server.ts
+++ src/server.ts
@@ -14,6 +14,7 @@ import swaggerDocument from "@/api/docs/swagger.json"
 const app = express();
 DataSeed.run()
 
+app.set("trust proxy", 1);
 app.use(cookieParser());
 app.use(cors());
 app.use(helmet());
